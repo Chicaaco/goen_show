46e5a2d89da091c2d921c07b51fb54ee069b0f88
Chika Nakamura <chikanakamura@192.168.1.201>
2025-07-10 20:52:50 +0100

    Update all files

--- script.js
+++ script.js
@@ -3,12 +3,15 @@ const modalContents = {
     <h2>About Goen</h2>
     <p>Goen is a human-centred cultural connection platform that facilitates real-life experiences between Japan-related hosts and curious guests in London.</p>
     <p>* “Goen” means “connection” in Japanese</p>
-    <img src="images/UI.png" alt="Examples of User Interface" style="width:100%; max-width:500px;">
+    <img src="images/UI.png" alt="Examples of User Interface" style="width:100%;">
   `,
   modal2: `
     <h2>Storyboard</h2>
     <p>This video shows what Goen’s service experience looks like through our user’s eyes.</p>
-    <video controls style="width:100%; max-width:600px;">
+    <video 
+    controls 
+    poster="images/Storyboard_Thumbnail.jpg" 
+    style="width:100%;">
       <source src="videos/Storyboard.mp4" type="video/mp4">
       Your browser does not support the video tag.
     </video>
@@ -16,23 +19,32 @@ const modalContents = {
   modal3: `
     <h2>Prototype Test</h2>
     <p>We ran an MVP experience where guests joined a local Japanese home for a shared dinner, and collected feedback on how it felt.</p>
-    <video controls style="width:100%; max-width:500px;">
-    <source src="videos/Prototype_Test.mp4" type="video/mp4">
-    Your browser does not support the video tag.
-  </video>
+    <video 
+    controls 
+    poster="images/Prototype_Thumbnail.jpg" 
+    style="width:100%;">
+      <source src="videos/Prototype_Test.mp4" type="video/mp4">
+      Your browser does not support the video tag.
+    </video>
   `,
   modal4: `
     <h2>System Map</h2>
-    <img src="images/System_Map.jpg" alt="System Map" style="width:100%; max-width:500px;">
+    <p>Overview of how our service ecosystem works.</p>
+    <img src="images/System_Map.jpg" alt="System Map" style="width:100%;">
   `,
   modal5: `
     <h2>Further Business Model</h2>
     <p>Looking ahead, our long-term vision involves building strategic partnerships with Japanese companies.</p>
     <p>By offering sponsorships, or product or material support, they can use Goen as a platform for local brand engagement, marketing, and real-world product trials. For example, a well-known Japanese cafe brand not yet in London could host a pop-up experience, letting guests try something they could only find in Japan.</p>
-    <img src="images/System_Map_Long.jpg" alt="Business Model" style="width:100%; max-width:500px;">
+    <img src="images/System_Map_Long.jpg" alt="Business Model" style="width:100%;">
   `,
   modal6: `
-    <h2>Contact / Follow Us</h2>
+  <h2>Impact</h2>
+  <p>Impact on each stakeholders.</p>
+  <img src="images/Impact.jpg" alt="Impact" style="width:100%;">
+  `,
+  modal7: `
+    <h2>Contact</h2>
     <p>Akane Nemoto <a href="mailto:10044370@network.rca.ac.uk">10044370@network.rca.ac.uk</a></p>
     <p>Chika Nakamura <a href="mailto:10045475@network.rca.ac.uk">10045475@network.rca.ac.uk</a></p>
   `
