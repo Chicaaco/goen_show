75f29a44939becc7fc0a6c38bb62d3f335d36411
Chika Nakamura <chikanakamura@ChikanoMacBook-Air.local>
2025-07-11 16:25:03 +0100

    Update all files

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const modalContents = {
     <h2>About Goen</h2>
     <p class="modal-text">Goen is a human-centred cultural connection platform that facilitates real-life experiences between Japan-related hosts and curious guests in London.</p>
     <p class="modal-text">* “Goen” means “connection” in Japanese</p>
-    <img src="images/UI.png" alt="Examples of User Interface" style="width:100%;" class="zoomable-img">
+    <img src="images/UI.png" class="zoomable-img" alt="Examples of User Interface" style="width:100%;">
   `,
   modal2: `
     <h2>Storyboard</h2>
@@ -31,13 +31,13 @@ const modalContents = {
     <h2>System Map</h2>
     <p class="modal-text">Overview of how our service ecosystem works.</p>
     <p class="modal-text">A seamless and reassuring onboarding process, including guest verification and host screening, and combined with early communication and post-experience feedback, helps both guests and hosts feel confident, safe, and connected.</p>
-    <img src="images/System_Map.jpg" alt="System Map" style="width:100%;" class="zoomable-img">
+    <img src="images/System_Map.jpg" class="zoomable-img" alt="System Map" style="width:100%;">
   `,
   modal5: `
     <h2>Further Business Model</h2>
     <p class="modal-text">Looking ahead, our long-term vision involves building strategic partnerships with Japanese companies.</p>
     <p class="modal-text">By offering sponsorships, or product or material support, they can use Goen as a platform for local brand engagement, marketing, and real-world product trials. For example, a well-known Japanese cafe brand not yet in London could host a pop-up experience, letting guests try something they could only find in Japan.</p>
-    <img src="images/System_Map_Long.jpg" alt="Business Model" style="width:100%;" class="zoomable-img">
+    <img src="images/System_Map_Long.jpg" class="zoomable-img" alt="Business Model" style="width:100%;">
   `,
   modal6: `
     <h2>Impact</h2>
@@ -49,6 +49,7 @@ const modalContents = {
     <p class="modal-text">Akane Nemoto <a href="mailto:10044370@network.rca.ac.uk">10044370@network.rca.ac.uk</a></p>
     <p class="modal-text">Chika Nakamura <a href="mailto:10045475@network.rca.ac.uk">10045475@network.rca.ac.uk</a></p>
     <img src="images/RCA_Logo.jpg" alt="Royal College of Art Logo" style="width:30%;">
+    <img src="images/SD_Logo.jpg" alt="Royal College of Art Service Design Logo" style="width:30%;">
   `,
 };
 
@@ -58,6 +59,16 @@ function openModal(event, modalKey) {
   modalContent.innerHTML = `<span class="close" onclick="closeModal()">&times;</span>` + modalContents[modalKey];
 
   modal.style.display = "block";
+
+
+ document.querySelectorAll('.zoomable-img').forEach(img => {
+  img.addEventListener('click', function () {
+    const overlay = document.getElementById('image-overlay');
+    const overlayImg = overlay.querySelector('img');
+    overlayImg.src = this.src;
+    overlay.style.display = 'flex';
+   });
+});
 }
 
 function closeModal() {
